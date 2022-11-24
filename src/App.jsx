4b28a11276b1e5bfaa240e588332c418f9debdd8
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-11-24 10:56:18 -0300

    Adicionado arquivo theme com o tema de cores da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
       <h1>ICMP Consultoria</h1>
-    </>
+    </ThemeProvider>
   )
 }
---
+++ src/styles/theme/default.js
@@ -0,0 +1,8 @@
+export const defaultTheme = {
+    body: "#FFF",
+    inside: "rgb(245, 245, 245)",
+    text: "#000",
+
+    blue: "rgb(1, 81, 134)",
+    red: "rgb(252, 1, 0)"
+};
