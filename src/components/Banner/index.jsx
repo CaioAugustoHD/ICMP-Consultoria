6fd54592322fc4d8328a358d5701286db515fbc0
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-11-27 11:14:56 -0300

    Adicionada estilização e imagem no banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { BannerStyled, TextBannerContainer, Title, Subtitle } from "./style";
+import { BannerStyled, TextBannerContainer, Title, Subtitle, ImgBanner } from "./style";
+
+import imgbanner from "../../assets/imgbanner.png";
 
 export function Banner(){
     return (
         <BannerStyled>
 
             <TextBannerContainer>
-                <Title>Vamos fazer uma gestão em TI completa para sua empresa</Title>
+                <Title>Vamos fazer uma <span>gestão em TI completa</span> para sua empresa</Title>
                 <Subtitle>Suporte remoto e presencial para TI, consultorias e projetos</Subtitle>
             </TextBannerContainer>
 
             <figure>
-                <img src=""/>
+                <ImgBanner src={imgbanner}/>
             </figure>
 
         </BannerStyled>
--- src/components/Banner/style.js
+++ src/components/Banner/style.js
@@ -5,6 +5,7 @@ export const BannerStyled = styled.main`
     margin-top: 69px;
     height: 500px;
     background-image: url(${banner});
+    background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
     display: flex;
@@ -12,12 +13,32 @@ export const BannerStyled = styled.main`
 
 export const TextBannerContainer = styled.div`
     color: #FFF;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const Title = styled.h1`
     max-width: 600px;
+    font-size: 2.8em;
+    text-shadow: 0 0 2px ;
+    line-height: 1.2em;
+    margin-bottom: 0.4em;
+
+    span {
+        color: ${({theme}) => theme.blue};
+        -webkit-text-stroke: 0.5px #FFF;
+        text-stroke: 1px #FFF;
+}
 `;
 
 export const Subtitle = styled.h2`
     max-width: 600px;
+    font-size: 1.3em;
+`;
+
+export const ImgBanner = styled.img`
+    height: 500px;
 `;
