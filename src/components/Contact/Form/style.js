d8364f4e0ae5b7f4a54798ddd97db9f80ffac3d2
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-12-07 23:43:41 -0300

    Media query para responsividade dos inputs em Contact

--- src/components/Contact/Form/style.js
+++ src/components/Contact/Form/style.js
@@ -5,6 +5,7 @@ export const FormStyled = styled.form`
     flex-wrap: wrap;
     max-width: 700px;
     margin: auto;
+    justify-content: center;
 `;
 
 export const InputContainer = styled.div`
@@ -18,6 +19,7 @@ export const InputContainer = styled.div`
         border-radius: 15px;
         border: none;
         box-shadow: 0 1px 3px rgba(0, 0, 0, 0.6);
+        min-width: 300px;
             
         :focus {
             outline: 1px solid #000;
@@ -27,19 +29,33 @@ export const InputContainer = styled.div`
     label {
         margin: 0 0 2px 8px;
     }
+
+    @media (max-width: 785px) {
+        width: 80%;
+    }
 `;
 
 export const SelectContainer = styled(InputContainer)`
     width: 100%;
+
+    @media (max-width: 785px) {
+        width: 80%;
+    }
 `;
 
 export const TextareaContainer = styled(SelectContainer)`
     padding: 1.1em 1em;
+    width: 100%;
 
     textarea {
         max-width: 100%;
         min-width: 100%;
         min-height: 33px;
+    }
+
+    @media (max-width: 785px) {
+        max-width: 80%;
+        min-width: 300px;
     }
 `;
 
