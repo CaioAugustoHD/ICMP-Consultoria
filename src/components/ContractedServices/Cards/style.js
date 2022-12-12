8e8add5a1857b92eb802cf468f0d9b706de3d1cd
CaioAugustoHD <caioaugustosbs@gmail.com>
2022-12-12 17:36:00 -0300

    Adicionada responsividade no componente ContractedServices

--- src/components/ContractedServices/Cards/style.js
+++ src/components/ContractedServices/Cards/style.js
@@ -2,13 +2,24 @@ import styled from "styled-components";
 
 export const CardStyled = styled.div`
     display: flex;
+    flex-wrap: wrap;
     justify-content: space-between;
     margin: 2em 0;
+
+    @media (max-width: 950px) {
+        width: 450px;
+        margin: 2em auto;
+        gap: 2em;
+    }
+
+    @media (max-width: 600px) {
+        width: 250px;
+    }
 `;
 
 export const Card = styled.div`
     text-align: center;
-    width: 25%;
+    width: 28%;
 
     p {
         margin-top: 0.5em;
@@ -16,5 +27,13 @@ export const Card = styled.div`
 
     img {
         height: 72px;
+    }
+
+    @media (max-width: 950px) {
+        width: 450px;
+    }
+
+    @media (max-width: 600px) {
+        width: 250px;
     }
 `;
